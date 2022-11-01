73ecf4db09eaf20502a5e898c690bc67c6551795
QQW <pavel.che411@gmail.com>
2022-11-01 15:38:32 +0300

    QW

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,4 +2,10 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    @Override
+    public String toString() {
+        return null;
+
+    }
 }
